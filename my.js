1d1d8ccaad89fdbf8e16efffd391b1e58966c760
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 15:53:04 -0800

    button

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 var mobileMenuVisible = false;
-  $('.hamburger-button').on('touchstart', function() {
+  $('.hamburger-button').on('click', function() {
       if (mobileMenuVisible) {
         $('nav.hamburger-menu-list').fadeOut('fast');
         mobileMenuVisible = false;
@@ -8,7 +8,7 @@ var mobileMenuVisible = false;
       $('nav.hamburger-menu-list').fadeIn('fast');
       mobileMenuVisible = true;
   });
-  $('nav.hamburger-menu-list').on('touchstart', function() {
+  $('nav.hamburger-menu-list').on('click', function() {
     $(this).css({'display':'none'});
     mobileMenuVisible = false;
   });
