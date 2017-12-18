cac1576617aa15585a8afc8ae0e99a1fc9547f60
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 12:55:18 -0800

    updated click

--- js/my.js
+++ js/my.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   var mobileMenuVisible = false;
-    $('.hamburger-button').click(function () {
+    $(".hamburger-button").click(function () {
         if (mobileMenuVisible) {
-          $('.mobile-menu').css({'display':'none'});
+          $('.mobile-menu').fadeOut('fast');
           mobileMenuVisible = false;
           return;
         }
-        $('.mobile-menu').css({'display':'block'});
+        $('.mobile-menu').fadeIn('fast');
         mobileMenuVisible = true;
     });
     $('.mobile-menu').click(function() {
@@ -14,3 +14,24 @@ $(document).ready(function(){
       mobileMenuVisible = false;
     });
 });
+//
+// $(document).ready(function(){
+//   $('.mobile-menu').hide();
+//   var mobileMenuVisible = false;
+//     $('.hamburger-button').click(function () {
+//       if (!mobileMenuVisible) {
+//         $('.mobile-menu').fadeIn('fast');
+//         mobileMenuVisible = true;
+//       }
+//     $('.mobile-menu').click(function() {
+//       $(this).css({'display':'none'});
+//     });
+// });
+//
+// $('.hamburger-menu-list').hide();
+// $('.hamburger-button').click(function() {
+//   $('div.panel-content').fadeOut('slow');
+//   $('.hamburger-menu-list').fadeIn('fast', function() {
+//     // $('.hamburger-menu-list').hide();
+//   });
+// });
