a373dc7f4efaad5a5f255b3bd5353774cd09e4fd
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 12:36:18 -0800

    click event

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var mobileMenuVisible = false;
-    $(".hamburger-button").on('click', function () {
+    $('.hamburger-button').click(function () {
         if (mobileMenuVisible) {
           $('.mobile-menu').css({'display':'none'});
           mobileMenuVisible = false;
