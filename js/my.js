ff65b73790f40bbab66a9dd21725441f700cd263
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 12:05:10 -0800

    updated code

---
+++ js/my.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  var mobileMenuVisible = false;
+    $(".hamburger-button").click(function () {
+        if (mobileMenuVisible) {
+          $('.mobile-menu').css({'display':'none'});
+          mobileMenuVisible = false;
+          return;
+        }
+        $('.mobile-menu').css({'display':'block'});
+        mobileMenuVisible = true;
+    });
+    $('.mobile-menu').click(function() {
+      $(this).css({'display':'none'});
+      mobileMenuVisible = false;
+    });
+});
