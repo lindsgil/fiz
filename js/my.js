24b219777f3664ffa5c27a2d608bfe5a77360174
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 12:58:58 -0800

    trying tagainnnn

--- js/my.js
+++ js/my.js
@@ -1,37 +1,14 @@
-$(document).ready(function(){
-  var mobileMenuVisible = false;
-    $(".hamburger-button").click(function () {
-        if (mobileMenuVisible) {
-          $('.mobile-menu').fadeOut('fast');
-          mobileMenuVisible = false;
-          return;
-        }
-        $('.mobile-menu').fadeIn('fast');
-        mobileMenuVisible = true;
-    });
-    $('.mobile-menu').click(function() {
-      $(this).css({'display':'none'});
-      mobileMenuVisible = false;
-    });
-});
-//
-// $(document).ready(function(){
-//   $('.mobile-menu').hide();
-//   var mobileMenuVisible = false;
-//     $('.hamburger-button').click(function () {
-//       if (!mobileMenuVisible) {
-//         $('.mobile-menu').fadeIn('fast');
-//         mobileMenuVisible = true;
-//       }
-//     $('.mobile-menu').click(function() {
-//       $(this).css({'display':'none'});
-//     });
-// });
-//
-// $('.hamburger-menu-list').hide();
-// $('.hamburger-button').click(function() {
-//   $('div.panel-content').fadeOut('slow');
-//   $('.hamburger-menu-list').fadeIn('fast', function() {
-//     // $('.hamburger-menu-list').hide();
-//   });
-// });
+var mobileMenuVisible = false;
+  $(".hamburger-button").click(function () {
+      if (mobileMenuVisible) {
+        $('.mobile-menu').fadeOut('fast');
+        mobileMenuVisible = false;
+        return;
+      }
+      $('.mobile-menu').fadeIn('fast');
+      mobileMenuVisible = true;
+  });
+  $('.mobile-menu').click(function() {
+    $(this).css({'display':'none'});
+    mobileMenuVisible = false;
+  });
