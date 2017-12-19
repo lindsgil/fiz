0c2f182145366b9790ca60b56698cea059fc33f3
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 16:27:59 -0800

    wah

--- my.js
+++ my.js
@@ -1,14 +1,15 @@
 var mobileMenuVisible = false;
-  $('button').on('touchstart click', function() {
-      if (mobileMenuVisible) {
-        $('.hamburger-menu-list').fadeOut('fast');
-        mobileMenuVisible = false;
-        return;
-      }
-      $('.hamburger-menu-list').fadeIn('fast');
-      mobileMenuVisible = true;
-  });
-  $('.hamburger-menu-list').on('touchstart click', function() {
-    $(this).css({'display':'none'});
+$(document).on('touchstart click', '.hamburger-button', function(e) {
+  e.stopPropagation();
+  if (mobileMenuVisible) {
+    $('.hamburger-menu-list').fadeOut('fast');
     mobileMenuVisible = false;
-  });
+    return;
+  }
+  $('.hamburger-menu-list').fadeIn('fast');
+  mobileMenuVisible = true;
+});
+$('.hamburger-menu-list').on('touchstart click', function() {
+  $(this).css({'display':'none'});
+  mobileMenuVisible = false;
+});
