ba957f4e13a4b266e6b5584a823383c1a81db582
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 17:57:07 -0800

    removed jquery

--- my.js
+++ my.js
@@ -1,20 +1,3 @@
-// var mobileMenuVisible = false;
-// $(document).on('touchstart click', '.hamburger-button', function(e) {
-//   console.log("IS THIS WORKING AT ALL");
-//   e.stopPropagation();
-//   if (mobileMenuVisible) {
-//     $('.hamburger-menu-list').fadeOut('fast');
-//     mobileMenuVisible = false;
-//     return;
-//   }
-//   $('.hamburger-menu-list').fadeIn('fast');
-//   mobileMenuVisible = true;
-// });
-// $('.hamburger-menu-list').on('touchstart click', function() {
-//   $(this).css({'display':'none'});
-//   mobileMenuVisible = false;
-// });
-
 function showNav() {
   let elem = document.getElementById('hamburger-menu-list');
   if(elem.style.display !== 'none') {
