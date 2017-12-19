ad264b5a547adaa10ddd1152c1a8f2e090f5f5d3
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-12-18 18:04:41 -0800

    maybe now

--- my.js
+++ my.js
@@ -1,5 +1,6 @@
 function showNav() {
   let elem = document.getElementById('hamburger-menu-list');
+  console.log('elem', elem);
   if(elem.style.display !== 'none') {
     elem.style.display = 'none';
   } else {
