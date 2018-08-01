2f75b6d0aae17d066a88a01959961f54a7db4d7f
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2018-07-31 22:43:03 -0700

    fiz updates

--- my.js
+++ my.js
@@ -1,13 +1,24 @@
 'use strict';
 
-var isShown = false;
-$('.hamburger-button').click(function() {
-  window.scrollTo(0, 0);
-  if (!isShown) {
-    $('.mobile-menu').fadeIn('fast');
-    isShown = true;
-  } else {
-    $('.mobile-menu').fadeOut('fast');
-    isShown = false;
+var menuOpen = false;
+
+function openNav() {
+  if (!menuOpen) {
+    document.getElementById("menu").style.height = "100%";
+    menuOpen = true;
   }
-});
+  else {
+    document.getElementById("menu").style.height = 0;
+    menuOpen = false;
+  }
+}
+
+var text = "Buy Tickets Now!";
+
+for(var i in text) {
+  if(text[i] === " ") {
+    $(".wavetext").append( $("<span>").html("&nbsp;") );
+  } else {
+    $(".wavetext").append( $("<span>").text(text[i]) );
+  }
+}
